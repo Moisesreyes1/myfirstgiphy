65d18a174a12bf5327d4ff421d2bb2e82a1198e1
unknown <mmreyes13@gmail.com>
2017-09-02 12:25:55 -0400

    Final commit

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,7 @@ var topics = ["Clouds", "Rain", "Forest"];
 // To display Nature Scenes //
 function displayNature() {
 	var topic = $(this).attr("data-name");
+console.log(topic);
 	var qeryURL = "https://api.giphy.com/v1/gifs/search?api_key=f2838b978f0b44b9bb70bbeff9f8aa05&q=" + topic + "&limit=10&offset=0&rating=G&lang=en";
 
 	// Create AJAX call for button being clicked //
@@ -12,9 +13,9 @@ function displayNature() {
 		method: "GET"
 
 	// Function after data from AJAX comes back //
-	})
-	.done(function(response) {
-		console.log(qeryURL);
+	}).done(function(response) {
+		//console.log("response");
+		console.log(response);
 	// Create div for nature scene //
 	var natureDiv = $("<div class='nature'>");
 
@@ -28,13 +29,20 @@ function displayNature() {
 	// natureDiv.append(pOne);
 
 	// To save the imageUrl //
-	var imageUrl = response.data.image_original_url;
+	var imageUrl = response.data["0"].images.fixed_height_still.url;
+	console.log(imageUrl);
 
+	// To save animate image //
+	var animation = response.data["0"].images.fixed_height.url;
 	// Create element to display image //
 	var natureImage = $("<img>");
 
 	// Setting the src attribute to the image //
 	natureImage.attr("src", imageUrl);
+	natureImage.attr("data-still", imageUrl);
+	natureImage.attr("data-animate", animation);
+	natureImage.attr("data-state", "imageUrl");
+	natureImage.addClass("natureImage");
 	natureImage.attr("alt", "nature image");
 
 	// Append nature image  //
@@ -50,6 +58,7 @@ function displayNature() {
 
 		// For loop to go through the array //
 		for (var i = 0; i < topics.length; i++) {
+			console.log(topics[i]);
 			// To dynamically generate buttons //
 			var a = $("<button>");
 			
@@ -82,7 +91,23 @@ function displayNature() {
 	});
 
 	// Add click event listener to all elements with class nature //
-	$(document).on("click", ".nature", displayNature);
+	$(document).on("click", ".topic", displayNature)
 
 	// Call renderButtons function to display initial buttons //
 	renderButtons();
+
+
+	// Click event to animate and pause images //
+	$(document).on("click", ".natureImage", function() {
+		var yes = $(this).attr("data-state");
+		console.log(this);
+		if(yes == "still"){
+			$(this).attr("src", $(this).data("animate"));
+			$(this).attr("data-state", "animate");
+		}
+
+		else {
+			$(this).attr("src", $(this).data("still"));
+			$(this).attr("data-state", "still");
+		}
+	});
